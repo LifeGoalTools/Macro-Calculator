4b32dd568afda41eed3eda367c0dfbd8fc9e7ba6
LifeGoalTools <calvinjp07@gmail.com>
2026-01-22 16:03:43 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'macro-v13';
+const cacheName = 'macro-v14';
 const assets = [
   './',
   './index.html',
@@ -25,6 +25,7 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+
 
 
 
