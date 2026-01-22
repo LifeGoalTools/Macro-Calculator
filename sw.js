7fecb172d57571ad0e548f9f8ab2960c0411b29c
Karumoru <calvinjp07@gmail.com>
2026-01-22 11:24:45 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'macro-cache-v3';
+const cacheName = 'macro-cache-v4';
 const assets = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
@@ -14,3 +14,4 @@ self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 
 });
+
